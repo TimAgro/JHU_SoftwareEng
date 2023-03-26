fb3e2859a7ffa89167223bd4fdbd8c70ddacd22a
asmoore <asmoor@gmail.com>
2023-03-26 10:54:54 +0200

    Phaser added
    Added Phaser and assets

--- app.py
+++ app.py
@@ -23,7 +23,6 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
-
 class Room(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
